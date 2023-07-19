a7d042d257a7eb385f5b3518c4e0d4997600263e
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-19 16:18:29 +0200

    Update to 4

--- src/test/java/example/micronaut/TestControllerTest.java
+++ src/test/java/example/micronaut/TestControllerTest.java
@@ -1,29 +1,53 @@
 package example.micronaut;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
+import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
-import jakarta.inject.Inject;
+import java.util.Collections;
+import java.util.Map;
 
-@MicronautTest // <1>
-public class TestControllerTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    @Inject
-    @Client("/")  // <2>
-    HttpClient client;
-
+@MicronautTest
+class TestControllerTest {
     @Test
-    public void testHello() {
-        HttpRequest<String> request = HttpRequest.GET("/hello");  // <3>
-        String body = client.toBlocking().retrieve(request);
+    void commonV1(@Client("/") HttpClient httpClient) {
+        BlockingHttpClient client = httpClient.toBlocking();
+        HttpRequest<?> request = createRequest("/common", "1");
+        HttpResponse<Map<String, String>> rsp = client.exchange(request, Argument.mapOf(String.class, String.class));
+        assertResponse(rsp, "common from v1");
 
+        request = createRequest("/common", null);
+        rsp = client.exchange(request, Argument.mapOf(String.class, String.class));
+        assertResponse(rsp, "common from v1");
+
+        request = createRequest("/common", "2");
+        rsp = client.exchange(request, Argument.mapOf(String.class, String.class));
+        assertResponse(rsp, "common from v2");
+
+        request = createRequest("/new", "2");
+        rsp = client.exchange(request, Argument.mapOf(String.class, String.class));
+        assertResponse(rsp, "new from v2");
+    }
+
+    HttpRequest<?> createRequest(String path, String apiVersion) {
+        return apiVersion == null ?
+                HttpRequest.GET(path) :
+                HttpRequest.GET(path).header("X-API-VERSION", apiVersion);
+    }
+
+    void assertResponse(HttpResponse<Map<String, String>> rsp, String expected) {
+        assertEquals(HttpStatus.OK, rsp.getStatus());
+        Map<String, String> body = rsp.body();
         assertNotNull(body);
-        assertEquals("Hello World", body);
+        assertEquals(1, body.keySet().size());
+        assertEquals(expected, body.get("message"));
     }
 }
